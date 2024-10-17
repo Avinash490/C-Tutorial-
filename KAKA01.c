ca8eeab64ca1886f46d40412fc2ecdaaee1fd4ed
Avinash Pratap Singh <avinashpratapsingh9389@gmail.com>
2024-10-17 18:44:46 +0530

    Add files via upload

---
+++ KAKA01.c
@@ -0,0 +1,19 @@
+// Day : 01 WAP to sum of numbers?
+#include<stdio.h>
+
+int main()
+{
+	int num1, num2, num3, sum;
+	
+	printf("Enter the value of number 1 : \n");
+	scanf("%d", &num1);
+	printf("Enter the value of number 2 : \n");
+	scanf("%d", &num2);
+	printf("Enter the value of number 3 : \n");
+	scanf("%d", &num3);
+
+	sum = num1+num2+num3;
+	printf("Sum of three number is = %d \n", sum);
+	
+	return 0;
+}
