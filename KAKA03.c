c1c68931ddcbaca1bc731421e0a1cbe8f4f75018
Avinash Pratap Singh <avinashpratapsingh9389@gmail.com>
2024-10-19 18:52:57 +0530

    Add files via upload

---
+++ KAKA03.c
@@ -0,0 +1,20 @@
+// Day : 03 WAP to multiply of two numbers?
+
+#include<stdio.h>
+
+int main()
+{
+	int num1, num2, num3, mul;
+	
+	printf("Enter the value of number 1 : \n");
+	scanf("%d", &num1);
+	printf("Enter the value of number 2 : \n");
+	scanf("%d", &num2);
+	printf("Enter the value of number 3 : \n");
+	scanf("%d", &num3);
+	
+	mul = num1 * num2 * num3;
+	printf("Multiple of three number is = %d \n", mul);
+
+	return 0;
+}
