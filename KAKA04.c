963cd27e5516a4e7211e32291df880b40702e8e8
Avinash Pratap Singh <avinashpratapsingh9389@gmail.com>
2024-10-20 19:09:59 +0530

    Add files via upload

---
+++ KAKA04.c
@@ -0,0 +1,19 @@
+// Day : 04 WAP to divide of numbers?
+#include<stdio.h>
+
+int main()
+{
+	int num1, num2, num3, div;
+	
+	printf("Enter the value of number 1 : \n");
+	scanf("%d", &num1);
+	printf("Enter the value of number 2 : \n");
+	scanf("%d", &num2);
+	printf("Enter the value of number 3 : \n");
+	scanf("%d", &num3);
+	
+	div = num1 / num2 / num3;
+	printf("Divide of three number is = %d \n", div);
+
+	return 0;
+}
