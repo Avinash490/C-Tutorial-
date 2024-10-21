a84395767338dbd78e6a5660e47835e4fa517f75
Avinash Pratap Singh <avinashpratapsingh9389@gmail.com>
2024-10-21 20:27:22 +0530

    Add files via upload

---
+++ KAKA05.c
@@ -0,0 +1,12 @@
+// Day : 05 WAP to demonstrate the function of Implicit Type Convertion? 
+
+#include<stdio.h>
+
+int main()
+{
+	int a = 7.47;
+	
+	printf("Example of implicit type convertion : It automatically convert the float (7.47) into integer: %d \n", a);
+
+	return 0;
+}
