99287edc0e596ecdc32acd186e99d09f72639aa1
Avinash Pratap Singh <avinashpratapsingh9389@gmail.com>
2024-10-22 21:32:10 +0530

    Add files via upload

---
+++ KAKA06.c
@@ -0,0 +1,13 @@
+// Day : 06 WAP to declare explicit type conversion?
+
+#include<stdio.h>
+
+int main()
+{
+	int a = 5, b;
+	
+	b = (float)a;
+	printf(" Example of explicit type convertion : %f \n", (float)b);
+
+	return 0;
+}
