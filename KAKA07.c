1d482dad74d67630d3e7402433255b2535270723
Avinash Pratap Singh <avinashpratapsingh9389@gmail.com>
2024-10-23 19:21:39 +0530

    Add files via upload

---
+++ KAKA07.c
@@ -0,0 +1,15 @@
+// Day : 07 WAP to demontrate type conversion?
+
+#include<stdio.h>
+
+int main()
+{
+	float a = 4.5, b = 5.5, c;
+	
+	c = a+b;
+	printf(" The value of c is %f \n ", c);
+	c = a+b;
+	printf(" The value of c is %d \n ", (int)c);
+
+	return 0;
+}
