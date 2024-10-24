ea905853a258bfe8c3f875e7b60553c0c6c0a91c
Avinash Pratap Singh <avinashpratapsingh9389@gmail.com>
2024-10-24 20:32:12 +0530

    Add files via upload

---
+++ KAKA08.c
@@ -0,0 +1,23 @@
+// Day : 08 WAP to using getchar(); putchar(); getch(); getche(); ?
+
+#include<stdio.h>
+
+int main()
+{
+	char kaka;
+	
+	printf("Enter any character : \n");
+	kaka = getchar();
+	printf("Entered character is : \n");
+	putchar(kaka);
+
+	// printf("Enter any character \n");
+	// kaka = getch();
+	// printf("Entered the character is %c \n", kaka);
+
+	// printf("Enter any character \n");
+	// kaka = getche();
+	// printf("Entered the character is : %c \n", kaka);
+
+	return 0;
+}
