9a7dae6b37bf21c288fc7fe4d1a0a4a56976cd3b
Avinash Pratap Singh <avinashpratapsingh9389@gmail.com>
2024-10-25 20:53:10 +0530

    Add files via upload

---
+++ KAKA09.c
@@ -0,0 +1,20 @@
+// Day : 09 WAP to find the biggest number?
+
+#include<stdio.h>
+int main()
+{
+	int a, b;
+	
+	printf("Enter the value of a and b \n :");
+	scanf("%d %d", &a, &b);
+	if(a>b)
+	{
+		printf("a is biggest no. %d", a);
+	}
+	else
+	{
+		printf("b is biggest no. %d", b);
+	}
+
+	return 0;
+}
