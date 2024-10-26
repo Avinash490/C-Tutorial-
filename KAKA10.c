ea5a0d174f242bbd23cb2d5b4a24049c146a86ed
Avinash Pratap Singh <avinashpratapsingh9389@gmail.com>
2024-10-26 20:00:03 +0530

    Add files via upload

---
+++ KAKA10.c
@@ -0,0 +1,21 @@
+// Day : 10 WAP to find the given number is positive or negative?
+
+#include<stdio.h>
+
+int main()
+{
+	int a;
+	
+	printf("Enter the value of a :");
+	scanf("%d", &a);
+	if(a<0)
+	{
+		printf("a is a negative no");
+	}
+	else
+	{
+		printf("a is a positive no");
+	}
+
+	return 0;
+}
