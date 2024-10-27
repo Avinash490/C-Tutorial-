c4916b62be3c0cf9ced7e5d57d609898c8e43a98
Avinash Pratap Singh <avinashpratapsingh9389@gmail.com>
2024-10-27 20:17:48 +0530

    Add files via upload

---
+++ KAKA11.c
@@ -0,0 +1,21 @@
+// Day : 11 WAP to find the given number is odd and even?
+
+#include<stdio.h>
+
+int main()
+{
+	int a;
+	
+	printf("Enter the value of a: \n");
+	scanf("%d", &a);
+	if(a%2==0)
+	{
+		printf("a is even");
+	}
+	else
+	{
+		printf("a is odd");
+	}
+	
+	return 0;
+}
