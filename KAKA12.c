2c76648884104ec19812ceea19e87c0de93df7d4
Avinash Pratap Singh <avinashpratapsingh9389@gmail.com>
2024-10-28 20:14:34 +0530

    Add files via upload

---
+++ KAKA12.c
@@ -0,0 +1,21 @@
+// Day : 12 WAP to check the student are eligible or not to fill the sarkari form?
+
+#include<stdio.h>
+
+int main()
+{       
+	int student;
+	
+	printf("Enter the age of student:");
+	scanf("%d", &student);
+	if(student>=18)
+	{
+		printf("The student is eligible to fill the form");
+	}
+	else
+	{
+		printf("The student is not eligible to fill the form");
+	}
+
+ 	return 0;
+}
