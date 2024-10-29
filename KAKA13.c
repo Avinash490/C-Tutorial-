2372c9bb6f81e5968872a112b6bde1de22fca3a8
Avinash Pratap Singh <avinashpratapsingh9389@gmail.com>
2024-10-29 21:51:09 +0530

    Add files via upload

---
+++ KAKA13.c
@@ -0,0 +1,30 @@
+// Day : 13 WAP to calculate the electricity bill?
+
+#include<stdio.h>
+
+int main()
+{
+	float unit, bill;
+	
+	printf("Enter the value of unit:");
+	scanf("%f", &unit);
+	if(unit<=100)
+	{
+		bill = 4.5*unit;
+	}
+	else if(unit>100 && unit<=200)
+	{
+		bill = 5*unit;
+	}
+	else if(unit>200 && unit<=300)
+	{
+		bill = 5.5*unit;
+	}
+	else
+	{
+		bill = 6*unit;
+	}
+	printf("The bill is = %.2f", bill);
+
+	return 0;
+}
