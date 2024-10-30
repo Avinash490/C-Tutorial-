bac03c92be597be654311bcad55db26744d5b461
Avinash Pratap Singh <avinashpratapsingh9389@gmail.com>
2024-10-30 20:48:06 +0530

    Add files via upload

---
+++ KAKA14.c
@@ -0,0 +1,16 @@
+// Day : 14 WAP to calculate a salary?
+
+#include<stdio.h>
+
+int main()
+{
+	float da, bs, gs;
+	
+	printf("Enter the value of bs:");
+	scanf("%f", &bs);
+	da = bs*32/100;
+	gs = bs+da;
+	printf("The gs is = %f", gs);
+	
+	return 0;
+}
