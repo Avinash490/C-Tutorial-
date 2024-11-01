3e1a18e6f9cff300c13d26389c6f6d0f87f88198
Avinash Pratap Singh <avinashpratapsingh9389@gmail.com>
2024-11-01 20:37:51 +0530

    Add files via upload

---
+++ KAKA16.c
@@ -0,0 +1,47 @@
+// Day : 16 WAP to find the biggest number in four variables?
+
+#include<stdio.h>
+
+int main()
+{
+	int a,b,c,d;
+	
+	printf("Enter the value of a,b,c and d: \n");
+	scanf("%d%d%d%d", &a,&b,&c,&d);
+	if(a>b)
+	{
+		if(a>c)
+		{
+			if(a>d)
+			{
+				printf("a is biggest");
+			}
+			else
+			{
+				printf("d is biggest");
+			}
+		}
+
+	}
+	else if(b>c)
+	{
+		if(b>d)
+		{
+			printf("b is biggest");
+		}
+		else
+		{
+			printf("d is biggest");
+		}
+	}
+	else if(c>d)
+	{
+		printf("c is biggest");
+	}
+	else
+	{
+		printf("d is biggest");
+	}
+	
+	return 0;
+}
