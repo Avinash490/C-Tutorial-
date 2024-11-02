0d8eeda395a5e77ffa8768c3d19d3e6766ddda55
Avinash Pratap Singh <avinashpratapsingh9389@gmail.com>
2024-11-02 21:36:28 +0530

    Add files via upload

---
+++ KAKA17.c
@@ -0,0 +1,35 @@
+// Day : 17 WAP to find the biggest number in three variables?
+
+#include<stdio.h>
+
+int main()
+{
+	float a,b,c;
+	
+	printf("Enter the value of a,b and c: \n");
+	scanf("%f%f%f", &a,&b,&c);
+	if(a>b)
+	{
+		if(a>c)
+		{
+			printf("a is biggest");
+		}
+		else
+		{
+			printf("c is biggest");
+		}
+	}
+	else
+	{
+		if(b>c)
+		{
+			printf("b is biggest");
+		}
+		else
+		{
+			printf("c is biggest");
+		}
+	}
+
+	return 0;
+}
